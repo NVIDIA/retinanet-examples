1ff8c6a25489deec7fcd56c2f7e25c11bec041ef
Floris Chabert <fchabert@nvidia.com>
2019-03-28 15:06:51 -0400

    Avoid overflow for large batch sizes in box decoding

--- csrc/cuda/decode.cu
+++ csrc/cuda/decode.cu
@@ -80,7 +80,7 @@ int decode(int batch_size,
 
   for (int batch = 0; batch < batch_size; batch++) {
     auto in_scores = static_cast<const float *>(inputs[0]) + batch * scores_size;
-    auto in_boxes = static_cast<const float *>(inputs[1]) + batch * scores_size * 4 / num_classes;
+    auto in_boxes = static_cast<const float *>(inputs[1]) + batch * (scores_size / num_classes) * 4;
 
     auto out_scores = static_cast<float *>(outputs[0]) + batch * top_n;
     auto out_boxes = static_cast<float4 *>(outputs[1]) + batch * top_n;
