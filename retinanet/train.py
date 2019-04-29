c802e43adc0e0c56b1718f1c847b525a2caa7149
Prethvi Kashinkunti <pkashinkunti@nvidia.com>
2019-04-29 14:33:14 -0700

    Start transition to new AMP unified API

--- retinanet/train.py
+++ retinanet/train.py
@@ -4,7 +4,7 @@ import torch
 from torch.optim import SGD
 from torch.optim.lr_scheduler import LambdaLR
 from torch.utils.data import DataLoader
-from apex import fp16_utils
+from apex import amp, optimizers
 from apex.parallel import DistributedDataParallel
 from .backbones.layers import convert_fixedbn_model
 
@@ -16,18 +16,38 @@ from .infer import infer
 def train(model, state, path, annotations, val_path, val_annotations, resize, max_size, jitter, batch_size, iterations, val_iterations, mixed_precision, lr, warmup, milestones, gamma, is_master=True, world=1, use_dali=True, verbose=True, metrics_url=None, logdir=None):
     'Train the model on the given dataset'
 
+    '''
     # Prepare dataset
     if verbose: print('Preparing dataset...')
     data_iterator = (DaliDataIterator if use_dali else DataIterator)(
         path, jitter, max_size, batch_size, model.stride,
         world, annotations, training=True)
     if verbose: print(data_iterator)
+    '''
 
     # Prepare model
     nn_model = model
     model = convert_fixedbn_model(model)
     if torch.cuda.is_available():
         model = model.cuda()
+
+    # Setup optimizer and schedule
+    optimizer = SGD(model.parameters(), lr=lr, weight_decay=0.0001, momentum=0.9) 
+
+    model, optimizer = amp.initialize(model, optimizer,
+                                      opt_level = 'O2' if mixed_precision else 'O0',
+                                      keep_batchnorm_fp32 = True,
+                                      loss_scale = 128.0,
+                                      verbosity = is_master)
+
+    if world > 1: 
+        model = DistributedDataParallel(model, delay_allreduce=True)
+    model.train()
+
+    if 'optimizer' in state:
+        optimizer.load_state_dict(state['optimizer'])
+
+    '''
     if mixed_precision:
         model = fp16_utils.BN_convert_float(model.half())
     if world > 1:
@@ -40,12 +60,22 @@ def train(model, state, path, annotations, val_path, val_annotations, resize, ma
         optimizer = fp16_utils.FP16_Optimizer(optimizer, static_loss_scale=128., verbose=False)
     if 'optimizer' in state:
         optimizer.load_state_dict(state['optimizer'])
+    '''
+
 
     def schedule(train_iter):
         if warmup and train_iter <= warmup:
             return 0.9 * train_iter / warmup + 0.1
         return gamma ** len([m for m in milestones if m <= train_iter])
     scheduler = LambdaLR(optimizer.optimizer if mixed_precision else optimizer, schedule)
+
+    # Prepare dataset
+    if verbose: print('Preparing dataset...')
+    data_iterator = (DaliDataIterator if use_dali else DataIterator)(
+        path, jitter, max_size, batch_size, model.module.stride,
+        world, annotations, training=True)
+    if verbose: print(data_iterator)
+
 
     if verbose:
         print('    device: {} {}'.format(
@@ -69,8 +99,8 @@ def train(model, state, path, annotations, val_path, val_annotations, resize, ma
 
             # Forward pass
             profiler.start('fw')
-            if mixed_precision:
-                data = data.half()
+            #if mixed_precision:
+            #    data = data.half()
             optimizer.zero_grad()
             cls_loss, box_loss = model([data, target])
             del data
@@ -78,8 +108,10 @@ def train(model, state, path, annotations, val_path, val_annotations, resize, ma
 
             # Backward pass
             profiler.start('bw')
-            if mixed_precision: optimizer.backward(cls_loss + box_loss)
-            else: (cls_loss + box_loss).backward()
+            with amp.scale_loss(cls_loss + box_loss, optimizer) as scaled_loss:
+                scaled_loss.backward()
+            #if mixed_precision: optimizer.backward(cls_loss + box_loss)
+            #else: (cls_loss + box_loss).backward()
             optimizer.step()
 
             # Reduce all losses
@@ -152,4 +184,5 @@ def train(model, state, path, annotations, val_path, val_annotations, resize, ma
 
     if logdir is not None:
         writer.close()
+
 
