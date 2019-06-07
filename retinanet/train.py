7e27f57e172a3a33c6318666cab9b8bf87310bc9
Floris Chabert <fchabert@nvidia.com>
2019-06-07 13:53:06 -0400

    Update SummaryWriter parameters

--- retinanet/train.py
+++ retinanet/train.py
@@ -65,7 +65,7 @@ def train(model, state, path, annotations, val_path, val_annotations, resize, ma
         from tensorboardX import SummaryWriter
         if is_master and verbose:
             print('Writing TensorBoard logs to: {}'.format(logdir))
-        writer = SummaryWriter(log_dir=logdir)
+        writer = SummaryWriter(logdir=logdir)
 
     profiler = Profiler(['train', 'fw', 'bw'])
     iteration = state.get('iteration', 0)
