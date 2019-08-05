afd28004da159aaf372a2b2d8507111717934873
103yiran <1039105206@qq.com>
2019-08-05 09:34:51 +0800

    remove unused parameter

--- retinanet/dali.py
+++ retinanet/dali.py
@@ -11,12 +11,11 @@ from pycocotools.coco import COCO
 class COCOPipeline(pipeline.Pipeline):
     'Dali pipeline for COCO'
 
-    def __init__(self, batch_size, num_threads, path, coco, training, annotations, world, device_id, mean, std, resize, max_size, stride):
+    def __init__(self, batch_size, num_threads, path, training, annotations, world, device_id, mean, std, resize, max_size, stride):
         super().__init__(batch_size=batch_size, num_threads=num_threads, device_id = device_id, prefetch_queue_depth=num_threads, seed=42)
 
         self.path = path
         self.training = training
-        self.coco = coco
         self.stride = stride
         self.iter = 0
 
@@ -88,7 +87,7 @@ class DaliDataIterator():
             self.categories_inv = { k: i for i, k in enumerate(self.coco.getCatIds()) }
 
         self.pipe = COCOPipeline(batch_size=self.batch_size, num_threads=2, 
-            path=path, coco=self.coco, training=training, annotations=annotations, world=world, 
+            path=path, training=training, annotations=annotations, world=world, 
             device_id = torch.cuda.current_device(), mean=self.mean, std=self.std, resize=resize, max_size=max_size, stride=self.stride)
 
         self.pipe.build()
