176ba4230189436a64cecf4af1cced9520c12235
David de la Iglesia Castro <diglesia@gradiant.org>
2020-04-17 21:22:03 +0200

    Make infer argv[3] optional. Default "detection.png"

--- extras/cppapi/infer.cpp
+++ extras/cppapi/infer.cpp
@@ -17,10 +17,6 @@ using namespace std;
 using namespace cv;
 
 int main(int argc, char *argv[]) {
-	if (argc != 4) {
-		cerr << "Usage: " << argv[0] << " engine.plan image.jpg result.png" << endl;
-		return 1;
-	}
 
 	cout << "Loading engine..." << endl;
 	auto engine = retinanet::Engine(argv[1]);
@@ -106,7 +102,9 @@ int main(int argc, char *argv[]) {
 	}
 
 	// Write image
-	imwrite(argv[3], image);
-
+	string out_file = argc == 4 ? string(argv[3]) : "detections.png";
+	cout << "Saving result to " << out_file << endl;
+	imwrite(out_file, image);
+	
 	return 0;
 }
